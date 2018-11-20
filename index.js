a8f9450497ece3715fe1f2f3bf099d36a6769b70
andrewjones <aljones15@gmail.com>
2018-11-20 11:29:45 -0500

    json check

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function changeOrdersToArray(record, next) {
   return next;
 }
 
-function processRecords() {
+function processRecords(records) {
   const nextRecords = records.map(createNext);
   return nextRecords;
 }
--- run.js
+++ run.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const records = require("./data.json");
 const transformer = require("./index");
-
 const next = transformer.default(records);
 const diskString = JSON.stringify(next);
 const path = `./output/${Date.now()}.json`;
-fs.writeFile(path, diskString, 'utf8', () => console.log(`Wrote to Disk`))
+fs.writeFile(path, diskString, 'utf8', () => console.log(`Wrote to Disk ${path}`))
