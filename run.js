b6b3c3931a74d18b107b36d19e03f5444ab15d97
andrewjones <aljones15@gmail.com>
2018-11-20 11:27:09 -0500

    writing output

--- run.js
+++ run.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
 const records = require("./data.json");
 const transformer = require("./index");
 
 const next = transformer.default(records);
+const diskString = JSON.stringify(next);
+const path = `./output/${Date.now()}.json`;
+fs.writeFile(path, diskString, 'utf8', () => console.log(`Wrote to Disk`))
